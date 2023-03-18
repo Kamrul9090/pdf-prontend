c6d8fd664853cf3cfce4f703a180d0eefbb6f9b5
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2023-03-18 21:35:20 +0600

    added logo and title of apps

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import pdf from '../../assets/banner/banner (1).jpg'
+import pdf from '../../assets/banner/pdf.png'
+import './banner.css'
 const Banner = () => {
     return (
-        <div className={`flex flex-col justify-center p-10`}>
+        <div className={`flex flex-col justify-center p-10 bg`}>
             <div>
-                <img src={pdf} width={100} height={100} />
+                <img src={pdf} width={100} height={100} alt="" />
             </div>
             <div className='flex flex-col justify-center'>
                 <h1 className='text-4xl font-bold'>How to convert to PDF </h1>
