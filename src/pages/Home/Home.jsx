f08fa2742559046f6228f34158dd9ee9c08f3b2a
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2023-03-18 22:52:41 +0600

    added some change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Navbar from '../../shared/Navbar';
 import Banner from '../Banner/Banner';
-import Category from '../Category/Category';
 import CreatePdfFile from '../CreatePdfFile/CreatePdfFile';
 import DocxToPdf from '../DocxToPdf/DocxToPdf';
 import ImgToPdf from '../ImgToPdf/ImgToPdf';
@@ -10,7 +8,6 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <Category></Category>
             <ImgToPdf></ImgToPdf>
             <DocxToPdf></DocxToPdf>
             <CreatePdfFile></CreatePdfFile>
